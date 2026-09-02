5b7da58022f5d11d8fcc13406403469c7ba42f6d
shameega <shaysonday@gmail.com>
2026-09-02 14:35:18 +0200

    Update urls.py

--- BarcaUtdHub/urls.py
+++ BarcaUtdHub/urls.py
@@ -1,18 +1,40 @@
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
-
+from django.views.static import serve
 
 urlpatterns = [
-    path('', include('core.urls') ),
-    # path('shop/', shop, name='shop'),
+    path('', include('core.urls')),
     path('cart/', include('cart.urls')),
-
     path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
+]
+
+
+
+
+
+# from django.contrib import admin
+# from django.urls import path, include
+
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+
+# urlpatterns = [
+#     path('', include('core.urls') ),
+#     # path('shop/', shop, name='shop'),
+#     path('cart/', include('cart.urls')),
+
+#     path('items/', include('item.urls')),
+#     path('admin/', admin.site.urls),
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
